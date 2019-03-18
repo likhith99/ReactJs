fe54995e2d58a0888573c9e97e4b38ae4727f42f
likhith-aalla <likhith9999@gmail.com>
2019-03-18 14:36:40 -0400

    Updates

---
+++ precache-manifest.2193d791927f41c1607b4d5608476a82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.4e6a4b67.js"
+  },
+  {
+    "revision": "05fb6bde97d6792a8ed6",
+    "url": "/react/static/js/main.05fb6bde.chunk.js"
+  },
+  {
+    "revision": "3d516afc5610eafe0045",
+    "url": "/react/static/js/2.3d516afc.chunk.js"
+  },
+  {
+    "revision": "05fb6bde97d6792a8ed6",
+    "url": "/react/static/css/main.7ce537a8.chunk.css"
+  },
+  {
+    "revision": "354e167f30ed840078c760d996f661ec",
+    "url": "/react/index.html"
+  }
+];
